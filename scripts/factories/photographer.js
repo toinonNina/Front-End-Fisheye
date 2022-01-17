ccbe76219301677c509cf9196f5685e8a9b3bede
nina <zd.toinon@gmail.com>
2022-01-17 10:42:27 +0100

    ajout des information sur les photographes page accueil, link par id sur url pour chaque photographe

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,24 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, price, tagline, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const article = document.createElement('article');
+        const wayUrl = "photographer.html";
+        const url = `${wayUrl}?photographer=${id}`;
+        article.innerHTML = `
+        <a href=${url}>
+        <img src=${picture} class="profilpicture" alt="">
+        <h2 class="photographerprofil">${name}
+        </h2>
+        </a>
+        <div class="photographerinfo">
+        <p class="city">${city}, ${country}</p>
+        <p class ="tagline">${tagline}</p>
+        <p class="price">${price}€/jour</p>
+        </div>`;
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, getUserCardDOM };
 }
